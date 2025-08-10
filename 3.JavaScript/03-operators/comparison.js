85dd56b3dde59630914c17e9e95781312d881155
Abdur-Rahaman-Zamir <arzamir99@gmail.com>
2025-08-10 22:51:49 +0530

    Refactor comparison examples by removing unnecessary imports and comments

--- 3.JavaScript/03-operators/comparison.js
+++ 3.JavaScript/03-operators/comparison.js
@@ -1,6 +1,3 @@
-const Arithmetic = require('arithmetic-js');
-const calc = new Arithmetic();
-
 // Basic comparisons
 console.log(calc.evaluate("5 == 5"));   // true
 console.log(calc.evaluate("5 != 3"));   // true
@@ -8,7 +5,5 @@ console.log(calc.evaluate("7 > 10"));   // false
 console.log(calc.evaluate("3 < 8"));    // true
 console.log(calc.evaluate("5 >= 5"));   // true
 console.log(calc.evaluate("4 <= 6"));   // true
-
-// With variables
-console.log(calc.evaluate("x > y", { x: 10, y: 5 })); // true
-console.log(calc.evaluate("age >= 18", { age: 16 })); // false
+console.log(calc.evaluate("10 === '10'")); // false
+console.log(calc.evaluate("10 !== 10")); // false
