c4d4f1f876439c45ac22f9c8f2aa7c4781bd9838
Abdur-Rahaman-Zamir <arzamir99@gmail.com>
2025-08-10 23:29:32 +0530

    leared about assignment operators

---
+++ 3.JavaScript/03-operators/assignment.js
@@ -0,0 +1,20 @@
+let x = 10;   // Assign
+console.log(x); // 10
+
+x += 5; // x = x + 5
+console.log(x); // 15
+
+x -= 3; // x = x - 3
+console.log(x); // 12
+
+x *= 2; // x = x * 2
+console.log(x); // 24
+
+x /= 4; // x = x / 4
+console.log(x); // 6
+
+x %= 4; // x = x % 4
+console.log(x); // 2
+
+x **= 3; // x = x ** 3 (2 এর 3 ঘাত)
+console.log(x); // 8
