f20178d62c4333d86252b5bd7752e4c3997f2db0
Abdur-Rahaman-Zamir <arzamir99@gmail.com>
2025-08-10 22:42:13 +0530

    Learend about arithmetic operators

---
+++ 3.JavaScript/03-operators/arithmetic.js
@@ -0,0 +1,20 @@
+let x = 10;
+let y = 5;
+
+let z = x + y; // Addition
+console.log(`Addition: ${z}`); // Output: 15
+
+z = x - y; // Subtraction
+console.log(`Subtraction: ${z}`); // Output: 5
+
+z = x * y; // Multiplication
+console.log(`Multiplication: ${z}`); // Output: 50
+
+z = x / y; // Division
+console.log(`Division: ${z}`); // Output: 2
+
+z = x % y; // Modulus
+console.log(`Modulus: ${z}`); // Output: 0
+
+z = x ** y; // Exponentiation
+console.log(`Exponentiation: ${z}`); // Output: 100000
