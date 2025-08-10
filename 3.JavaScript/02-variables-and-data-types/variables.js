40b744c121183fb56006de17290179ae080fa3b6
Abdur-Rahaman-Zamir <arzamir99@gmail.com>
2025-08-10 17:08:06 +0300

    learned about keywords(let, const,var) form wasim

--- 3.JavaScript/02-variables-and-data-types/variables.js
+++ 3.JavaScript/02-variables-and-data-types/variables.js
@@ -1,6 +1,24 @@
-//const pi = 3.14;
-//console.log(pi);
-const pi = 3.14;
-console.log(pi);
-var radius = 5;
-var area = pi * radius * radius;
+//1.var key word
+var z; //variable declaration and initialization
+z = 50; //variable initialization
+z = 60; //variable re-assignment
+var z = 70; //re-declaration is allowed with var
+console.log(z);
+
+
+//2.let key word
+
+let x; //variable declaration
+x = 10; //variable initialization
+x = 20; //variable re-assignment
+// let x = 25; //re-declaration is not allowed with let
+
+console.log(x);
+
+
+//3.const key word
+const y = 30; //variable declaration and initialization
+console.log(y);
+// y = 40; //uncommenting this line will cause an error because const cannot be reassigned
+console.log(y)
+
