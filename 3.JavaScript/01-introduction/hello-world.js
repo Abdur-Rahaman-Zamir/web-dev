c3a4c19ab22c0251b2174bd751c3f61b54d8f997
Abdur-Rahaman-Zamir <arzamir99@gmail.com>
2025-08-10 12:14:25 +0300

    added extra veriable

--- 3.JavaScript/01-introduction/hello-world.js
+++ 3.JavaScript/01-introduction/hello-world.js
@@ -3,3 +3,12 @@ console.log("Hello, JavaScript!");
 
 // Another example: say your name
 console.log("I am learning JavaScript step by step.");
+
+
+// Example of variables
+
+let name = "John";
+let age = 30; 
+let className =  'b. tech. in computer science';
+// Print the variables
+console.log("My name is " + name + " and I am " + age + " years old in " + className + "."); ;
