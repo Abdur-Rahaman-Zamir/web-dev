930ebd7b157f6cc051857eb16711615d154fd88d
Abdur-Rahaman-Zamir <arzamir99@gmail.com>
2025-08-10 17:26:19 +0300

    all data types example one by one

---
+++ 3.JavaScript/02-variables-and-data-types/data type.js
@@ -0,0 +1,25 @@
+//data type are 7 types
+
+// 1. String 
+let name = "zamir"
+let age = 27
+console.log(name);
+console.log(age);
+//2. Number
+let number = 100;
+console.log(number);
+//3. BigInt 
+let bigNumber = 1234567890123456789012345678901234567890n; // BigInt with 'n' suffix
+console.log(bigNumber)
+//4. Boolean
+let isTrue = true; // or false
+console.log(isTrue);
+//5. Undefined
+let notDefined; // variable declared but not initialized
+console.log(notDefined); // Output: undefined
+//6. Null
+let emtyValue = null;
+console.log(emtyValue);
+//7. Symbol
+let uniqueSymbol = Symbol("unique");
+console.log(uniqueSymbol);
