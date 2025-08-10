c5f58c184d5285bdbf2770a1b232355d5d879589
Abdur-Rahaman-Zamir <arzamir99@gmail.com>
2025-08-10 12:54:35 +0300

    learning variable let

---
+++ 3.JavaScript/02-variables-and-data-types/variables.js
@@ -0,0 +1,8 @@
+//start variables of js 
+let name = "Abdur Rahman";
+console.log(name);
+let age = 25;
+console.log(age);
+let className = 'B.Tech in Computer Science';
+console.log(className);
+// end of variables of js
