3fb2fa33d24530f7853eff537adac1a4fd112172
Abdur-Rahaman-Zamir <arzamir99@gmail.com>
2025-08-13 08:14:30 +0530

    learned about if

---
+++ 3.JavaScript/04-control-flow/if...else if...else.js
@@ -0,0 +1,25 @@
+let marks = 90;
+if (marks >= 90) {
+    console.log("grade AA and scholarship");
+}
+else if (marks >= 80) {
+    console.log("grade A+ and excellent");
+}
+else if ("marks >=60") {
+    console.log("grade A");
+}
+else if (marks >= 50) {
+    console.log("grade B+ very good");
+}
+else if (marks >= 40) {
+    console.log("grade B and good");
+}
+else if (marks >= 30) {
+    console.log("grade C and pass");
+}
+else if (marks >= 25) {
+    console.log("grade D and pass");
+}
+else {
+    console.log("grade F and fail");
+}
