b62260d5ae79f31255d48b05146435490c878eac
Abdur-Rahaman-Zamir <arzamir99@gmail.com>
2025-08-13 21:40:21 +0530

    basic switch js

---
+++ 3.JavaScript/04-control-flow/switch.js
@@ -0,0 +1,28 @@
+let fruit = "apple";
+switch (fruit) {
+    case "apple":
+        console.log("apple is a red fruit");
+    case "banana":
+        console.log("banana is yellow fruit");
+    case ("pamegranate"):
+        console.log("pomegranate is a red fruit");
+        break;
+    default:
+        console.log("unknown fruit");
+}
+
+let day = 3;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  default:
+    console.log("Unknown day");
+}
