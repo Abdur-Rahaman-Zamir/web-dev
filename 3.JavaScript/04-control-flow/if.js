f767236f4e6907ebe7b40140d52e741687f8b6ab
Abdur-Rahaman-Zamir <arzamir99@gmail.com>
2025-08-13 20:54:40 +0530

    leared about if

--- 3.JavaScript/04-control-flow/if.js
+++ 3.JavaScript/04-control-flow/if.js
@@ -1,9 +1,11 @@
 //1. What is if in JavaScript?
 //if is a conditional statement — it lets your code make decisions.
 //It checks a condition (true or false) and runs some code only if that condition is true.
-let age = 20;
+let age = 16;
 if (age >=18){
     console.log("you are an adult");
+} else {
+    console.log ("you are a minor");
 }
 
 
