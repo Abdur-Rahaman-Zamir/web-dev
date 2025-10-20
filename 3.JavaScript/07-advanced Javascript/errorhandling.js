a5849ec4c0bfe6e8dc0fcc4c48408f56e84c1d44
Abdur-Rahaman-Zamir <arzamir99@gmail.com>
2025-10-20 20:28:07 +0530

    error handling

---
+++ 3.JavaScript/07-advanced Javascript/errorhandling.js
@@ -0,0 +1,30 @@
+setTimeout(() => {
+    console.log("hacking wifi...Plese wait....");
+}, 2000);
+
+try{
+console.log("zamir");
+
+}
+catch(error){
+    console.log("balle balle");
+}
+setTimeout(() => {
+    console.log("WiFi hacked successfully");
+}, 5000);  
+
+setTimeout(() => {
+    console.log("Getting password from database...");
+}   , 7000);
+
+setTimeout(() => {
+    console.log("Password found: 12345@abcd");
+}, 10000);   
+
+setTimeout(() => {
+    console.log("Connecting to WiFi...");
+}, 12000);
+
+setTimeout(() => {
+    console.log("Connected to WiFi successfully!");
+}, 15000);
